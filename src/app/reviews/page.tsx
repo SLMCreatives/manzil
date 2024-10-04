15c5c8ddcf7809fde1b50a25ef93ef3bfd4a7f56
SLMCreatives <sulaiman@slmcreatives.com>
2024-10-05 05:35:24 +0800

    fix: reviews

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -2,7 +2,7 @@
 
 import { ElfsightWidget } from "react-elfsight-widget";
 
-export function ReviewsPage() {
+export default function ReviewsPage() {
 	return (
 		<div className="w-full">
 			<ElfsightWidget widgetID="87259629-d79b-45c2-93ff-3af5086637d9" />
