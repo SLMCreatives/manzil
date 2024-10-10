ade709baeec6c4ed7f4bcbd45696cd59ec2d53d0
SLMCreatives <sulaiman@slmcreatives.com>
2024-10-10 12:40:48 +0800

    fix: utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -102,4 +102,114 @@ export const formatProductName = (name: string, variant?: string) => {
 	if (!variant) {
 		return name;
 	}
+	return `${name} (${deslugify(variant)})`;
+};
+
+export const calculateCartTotalPossiblyWithTax = (cart: {
+	cart: {
+		amount: number;
+		metadata?: { taxCalculationId?: string };
+	};
+	lines: Array<{ product: { default_price?: { unit_amount?: number | null } }; quantity: number }>;
+	shippingRate?: { fixed_amount?: { amount?: number } } | null;
+}) => {
+	if (!cart) {
+		return 0;
+	}
+	if (cart.cart.metadata?.taxCalculationId) {
+		return cart.cart.amount;
+	}
+
+	return (
+		(cart.shippingRate?.fixed_amount?.amount ?? 0) + calculateCartTotalNetWithoutShipping(cart)
+	);
+};
+
+export const calculateCartTotalNetWithoutShipping = (cart: {
+	cart: {
+		amount: number;
+		metadata?: { taxCalculationId?: string };
+	};
+	lines: Array<{ product: { default_price?: { unit_amount?: number | null } }; quantity: number }>;
+	shippingRate?: { fixed_amount?: { amount?: number } } | null;
+}) => {
+	if (!cart) {
+		return 0;
+	}
+
+	return cart.lines.reduce(
+		(total, { product, quantity }) => total + (product.default_price?.unit_amount ?? 0) * quantity,
+		0,
+	);
+};
+
+type Money = { amount: number; currency: string };
+
+export function invariant(condition: unknown, message: string): asserts condition {
+	if (!condition) {
+		throw new Error(message);
+	}
+}
+
+export const assertInteger = (value: number) => {
+	invariant(Number.isInteger(value), "Value must be an integer");
+};
+
+const getDecimalsForStripe = (currency: string) => {
+	invariant(currency.length === 3, "currency needs to be a 3-letter code");
+
+	const stripeDecimals = stripeCurrencies[currency.toUpperCase()];
+	const decimals = stripeDecimals ?? 2;
+	return decimals;
+};
+
+export const getStripeAmountFromDecimal = ({ amount: major, currency }: Money) => {
+	const decimals = getDecimalsForStripe(currency);
+	const multiplier = 10 ** decimals;
+	return Number.parseInt((major * multiplier).toFixed(0), 10);
+};
+
+export const getDecimalFromStripeAmount = ({ amount: minor, currency }: Money) => {
+	assertInteger(minor);
+	const decimals = getDecimalsForStripe(currency);
+	const multiplier = 10 ** decimals;
+	return Number.parseFloat((minor / multiplier).toFixed(decimals));
+};
+
+export const formatMoney = ({
+	amount: minor,
+	currency,
+	locale = "en-US",
+}: Money & { locale?: string }) => {
+	const amount = getDecimalFromStripeAmount({ amount: minor, currency });
+	return new Intl.NumberFormat(locale, {
+		style: "currency",
+		currency,
+	}).format(amount);
+};
+
+// https://docs.stripe.com/development-resources/currency-codes
+const stripeCurrencies: Record<string, number> = {
+	BIF: 0,
+	CLP: 0,
+	DJF: 0,
+	GNF: 0,
+	JPY: 0,
+	KMF: 0,
+	KRW: 0,
+	MGA: 0,
+	PYG: 0,
+	RWF: 0,
+	UGX: 0,
+	VND: 0,
+	VUV: 0,
+	XAF: 0,
+	XOF: 0,
+	XPF: 0,
+
+	BHD: 3,
+	JOD: 3,
+	KWD: 3,
+	OMR: 3,
+	TND: 3,
 };
